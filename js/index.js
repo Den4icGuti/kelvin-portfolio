66ead90df2afaa661b24988aeda41a58a7a9b95e
Den4icGuti <goos7412@gmail.com>
2023-12-01 20:11:53 +0200

    add section portfolio and index.js file

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const refs = {
+//   burgerPortfolio: document.querySelector(".js-portfolio-burger"),
+// };
+
+// const { burgerPortfolio } = refs;
+
+// const openBurgerMenu = () => {
+//   console.log("Open menu");
+// };
+
+// burgerPortfolio.addEventListener("click", openBurgerMenu);
