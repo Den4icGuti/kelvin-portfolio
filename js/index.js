8484e520ee702b6687e7eff4debd1652d7ebde08
Den4icGuti <goos7412@gmail.com>
2023-12-28 15:34:45 +0200

    Add modal window

--- js/index.js
+++ js/index.js
@@ -1,12 +1,61 @@
 const refs = {
   footerBtn: document.querySelector(".js-footer-btn"),
   modalMessage: document.querySelector(".js-modal-open"),
+  formMessage: document.querySelector(".js-msg-form"),
 };
 
-const { footerBtn, modalMessage } = refs;
+const bd = [];
 
-const openModalMessage = () => {
+const { footerBtn, modalMessage, formMessage } = refs;
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = e.currentTarget.email.value;
+  const message = e.currentTarget.message.value;
+
+  if (email === "" || message.length < 10) {
+    alert("error");
+    return;
+  } else {
+    const newData = {
+      email,
+      message,
+    };
+
+    const data = JSON.stringify(newData);
+
+    localStorage.setItem("newData", data);
+    bd.push(data);
+    console.log("new data", bd);
+    formMessage.reset();
+    onCloseForm();
+  }
+}
+
+formMessage.addEventListener("submit", onFormSubmit);
+
+function onOpenModal() {
   modalMessage.classList.add("show-modal-msg");
-};
+  window.addEventListener("keydown", onPressEsc);
+}
 
-footerBtn.addEventListener("click", openModalMessage);
+footerBtn.addEventListener("click", onOpenModal);
+
+function onCloseForm() {
+  modalMessage.classList.remove("show-modal-msg");
+  window.removeEventListener("keydown", onPressEsc);
+}
+
+function onBackdropClick(e) {
+  if (e.target === e.currentTarget) {
+    onCloseForm();
+  }
+}
+
+function onPressEsc(e) {
+  if (e.code === "Escape") {
+    onCloseForm();
+  }
+}
+
+modalMessage.addEventListener("click", onBackdropClick);
